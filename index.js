c6e58ea862372e79e7d68a58c7927730050cfa02
de3work <Simon.Detrois@mgm-tp.com>
2022-04-18 14:27:52 +0200

    index.js added

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('<h1>Hello World</h1>\n');
+  res.send('Hello World\n');
+});
+
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+
+  console.log('Your nodejs app is listening at port %s',  port);
 });
