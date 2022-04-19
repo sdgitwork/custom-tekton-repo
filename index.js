60c5da1b3d577dbff27c42c8bb115d9cdeae4145
de3work <Simon.Detrois@mgm-tp.com>
2022-04-19 10:52:26 +0200

    _test_

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('<h1 style="text-align:center;">TEST<h1>\n');
+  res.send('<h1 style="text-align:center;">_TEST_<h1>\n');
 });
 
 var server = app.listen(3000, function () {
