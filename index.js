b31ad6d2d91e329635ec0529397cdb5e07d2d41b
de3work <Simon.Detrois@mgm-tp.com>
2022-04-25 11:00:05 +0200

    changes

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('<h1 style="text-align:center;">not on main branch again<h1>\n');
+  res.send('<h1 style="text-align:center;">tekton test<h1>\n');
 
 });
 
-app.get('/hello', function (req, res) {
-  res.send('<h1 style="text-align:center;">/hello<h1>\n');
+app.get('/test', function (req, res) {
+  res.send('<h1 style="text-align:center;">/test<h1>\n');
   
 });
 
