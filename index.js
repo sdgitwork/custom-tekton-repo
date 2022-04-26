57d699f84556854bf230fac39ac31242c280ca3b
de3work <Simon.Detrois@mgm-tp.com>
2022-04-26 09:25:23 +0200

    nain branch trigger

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('<h1 style="text-align:center;">on main brnach again<h1>\n');
 });
 
+app.get('/main', function (req, res) {
+  res.send('<h1 style="text-align:center;">on main brnach again<h1>\n');
+});
+
 var server = app.listen(3000, function () {
   var port = server.address().port;
 
