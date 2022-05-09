cd23593644d133c2eeba0a4a2da41cae2d22badd
de3work <Simon.Detrois@mgm-tp.com>
2022-05-09 10:37:53 +0200

    trigger

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('<h1 style="text-align:center;">on main branch again !!!<h1>\n');
+  res.send('<h1 style="text-align:center;">on main branch again!!!<h1>\n');
 });
 
 app.get('/main', function (req, res) {
